367ef78ec363e9f833a2b28c981a3f071f87bcec
haru0l <86917622+haru0l@users.noreply.github.com>
2022-05-21 13:28:42 +0800

    Initial upload
    Add files

---
+++ login.php
@@ -0,0 +1,99 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "estudent";
+$connect = new mysqli($servername, $username, $password, $dbname);
+  
+if($_SERVER['REQUEST_METHOD'] === 'POST')  
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $query = "SELECT teacherPassword from teacher WHERE login_id='$username'";
+    $result = $connect->query($query);
+    $row = $result->fetch_assoc();
+    $count = mysqli_num_rows($result);
+    if ($count == 1)
+    {
+        session_start();
+        $cookie_name = "user_name";
+        $user_name_value = $username;
+        setcookie($cookie_name, $user_name_value, time() + (86400 * 30), "/");
+        header ('Location: index.html');
+    }
+    else 
+    {
+        echo "wrong username or password";
+    }
+}
+
+?>
+ 
+
+ <html>
+  <head>
+  	<title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="assets/vendor/font-awesome/css/all.min.css">
+	
+	<link rel="stylesheet" href="assets/css/login.css">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Login</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<div class="wrap d-md-flex">
+						<div class="img" style="background-image: url(assets/img/logo_sk.png);">
+			      </div>
+						<div class="login-wrap p-4 p-md-5">
+			      	<div class="d-flex">
+			      		<div class="w-100">
+			      			<h3 class="mb-4">Sign In</h3>
+			      		</div>
+			      	</div>
+							<form action="login.php" class="signin-form" method="post">
+			      		<div class="form-group mb-3">
+			      			<label class="label" for="name">Username</label>
+			      			<input type="text" name="username" class="form-control" placeholder="Username" required>
+			      		</div>
+		            <div class="form-group mb-3">
+		            	<label class="label" for="password">Password</label>
+		              <input type="password" name="password" class="form-control" placeholder="Password" required>
+		            </div>
+		            <div class="form-group">
+		            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
+		            </div>
+		            <div class="form-group d-md-flex">
+		            	<div class="w-50 text-left">
+									</div>
+									<div class="w-50 text-md-right">
+										<a href="#">Forgot Password</a>
+									</div>
+		            </div>
+		          </form>
+		        </div>
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+  <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+  <script src="assets/js/popper.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/main.js"></script>
+
+	</body>
+</html>
+
