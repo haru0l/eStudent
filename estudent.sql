f2aed682eea8934c706eb9711ea410e63eaae62f
haru0l <86917622+haru0l@users.noreply.github.com>
2022-06-16 01:00:57 +0800

    Update estudent.sql

--- estudent.sql
+++ estudent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2022 at 03:22 AM
+-- Generation Time: Jun 15, 2022 at 06:59 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -28,31 +28,31 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `grades` (
-  `stuIC` varchar(50) NOT NULL,
-  `marksBM` varchar(2) NOT NULL,
-  `marksBI` varchar(2) NOT NULL,
-  `marksMath` varchar(2) NOT NULL,
-  `marksSains` varchar(2) NOT NULL,
-  `marksSeni` varchar(2) NOT NULL,
-  `marksPI` varchar(2) NOT NULL,
-  `marksBA` varchar(2) NOT NULL,
-  `marksTasmik` varchar(2) NOT NULL,
-  `marksMusik` varchar(2) NOT NULL,
-  `marksRBT` varchar(2) NOT NULL,
-  `marksSejarah` varchar(2) NOT NULL,
+  `stuIC` varchar(12) NOT NULL,
+  `marksBM` varchar(3) NOT NULL,
+  `marksBI` varchar(3) NOT NULL,
+  `marksMath` varchar(3) NOT NULL,
+  `marksSains` varchar(3) NOT NULL,
+  `marksSeni` varchar(3) NOT NULL,
+  `marksPI` varchar(3) NOT NULL,
+  `marksBA` varchar(3) NOT NULL,
+  `marksTasmik` varchar(3) NOT NULL,
+  `marksMusik` varchar(3) NOT NULL,
+  `marksRBT` varchar(3) NOT NULL,
+  `marksSejarah` varchar(3) NOT NULL,
   `year` year(4) NOT NULL,
   `test` varchar(50) NOT NULL,
-  `bandBM` varchar(2) NOT NULL,
-  `bandBI` varchar(2) NOT NULL,
-  `bandMath` varchar(2) NOT NULL,
-  `bandSains` varchar(2) NOT NULL,
-  `bandSeni` varchar(2) NOT NULL,
-  `bandPI` varchar(2) NOT NULL,
-  `bandBA` varchar(2) NOT NULL,
-  `bandTasmik` varchar(2) NOT NULL,
-  `bandMusik` varchar(2) NOT NULL,
-  `bandRBT` varchar(2) NOT NULL,
-  `bandSejarah` varchar(2) NOT NULL,
+  `bandBM` varchar(3) NOT NULL,
+  `bandBI` varchar(3) NOT NULL,
+  `bandMath` varchar(3) NOT NULL,
+  `bandSains` varchar(3) NOT NULL,
+  `bandSeni` varchar(3) NOT NULL,
+  `bandPI` varchar(3) NOT NULL,
+  `bandBA` varchar(3) NOT NULL,
+  `bandTasmik` varchar(3) NOT NULL,
+  `bandMusik` varchar(3) NOT NULL,
+  `bandRBT` varchar(3) NOT NULL,
+  `bandSejarah` varchar(3) NOT NULL,
   `remarksBM` varchar(50) NOT NULL,
   `remarksBI` varchar(50) NOT NULL,
   `remarksMath` varchar(50) NOT NULL,
@@ -65,14 +65,6 @@ CREATE TABLE `grades` (
   `remarksRBT` varchar(50) NOT NULL,
   `remarksSejarah` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `grades`
---
-
-INSERT INTO `grades` (`stuIC`, `marksBM`, `marksBI`, `marksMath`, `marksSains`, `marksSeni`, `marksPI`, `marksBA`, `marksTasmik`, `marksMusik`, `marksRBT`, `marksSejarah`, `year`, `test`, `bandBM`, `bandBI`, `bandMath`, `bandSains`, `bandSeni`, `bandPI`, `bandBA`, `bandTasmik`, `bandMusik`, `bandRBT`, `bandSejarah`, `remarksBM`, `remarksBI`, `remarksMath`, `remarksSains`, `remarksSeni`, `remarksPI`, `remarksBA`, `remarksTaskmik`, `remarksMusik`, `remarksRBT`, `remarksSejarah`) VALUES
-('020822050567', '77', '66', '', '33', '67', '', '', '', '', '', '', 2022, 'PepAwal', '1', '1', '', '1', '1', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
-('020822050567', '', '', '', '', '23', '', '', '', '', '', '', 2022, 'PepAkhir', '', '', '', '1', '6', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -90,16 +82,9 @@ CREATE TABLE `report` (
   `cocurricular` varchar(20) NOT NULL,
   `rankingClass` varchar(20) NOT NULL,
   `rankingWhole` varchar(20) NOT NULL,
+  `test` varchar(50) NOT NULL,
   `year` year(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `report`
---
-
-INSERT INTO `report` (`stuIC`, `attitude`, `attendance`, `comment`, `teacherName`, `class`, `cocurricular`, `rankingClass`, `rankingWhole`, `year`) VALUES
-('4', 'among', 12, 'among us', '1234', '4 Bijak', '', '', '', 2022),
-('020822050567', 'amongus', 1, 'amongus', 'amongus', 'amongus', 'amongus', 'amongus', 'amongus', 2022);
 
 -- --------------------------------------------------------
 
@@ -109,15 +94,11 @@ INSERT INTO `report` (`stuIC`, `attitude`, `attendance`, `comment`, `teacherName
 
 CREATE TABLE `student` (
   `tableID` int(1) NOT NULL,
-  `icNum` varchar(14) NOT NULL,
+  `icNum` varchar(12) NOT NULL,
   `stuName` varchar(60) NOT NULL,
   `stuGender` varchar(10) NOT NULL,
-  `date_of_birth` varchar(10) NOT NULL,
-  `stuAddress` varchar(200) NOT NULL,
-  `stu_phoneNum` varchar(15) NOT NULL,
   `stuPassword` varchar(20) NOT NULL,
   `class` varchar(20) NOT NULL,
-  `cocurricular` varchar(20) NOT NULL,
   `year` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -125,11 +106,9 @@ CREATE TABLE `student` (
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`tableID`, `icNum`, `stuName`, `stuGender`, `date_of_birth`, `stuAddress`, `stu_phoneNum`, `stuPassword`, `class`, `cocurricular`, `year`) VALUES
-(1, '020822050567', 'Adam', 'Male', '22/08/22', 'Address', '0149454863', 'adam', '1 Bijak', 'cocurricular', '2022'),
-(2, '4', 'test 4b', 'Male', '4', '4', '4', 'amongus', '4 Bijak', '4', ''),
-(3, '9999', 'hi', 'Male', '', '', '', '', '1 Bijak', '', ''),
-(24, '2323', 'bangkai meow', 'Male', '23', '', 'meow', '', '1 Bijak', '', '');
+INSERT INTO `student` (`tableID`, `icNum`, `stuName`, `stuGender`, `stuPassword`, `class`, `year`) VALUES
+(2, '050104121427', 'XU YOO WEI', 'Lelaki', '', '1 Bijak', '2022'),
+(3, '050230503423', 'SYED AIMAN HASADI BIN CHE ZAINI', 'Lelaki', '', '1 Bijak', '2022');
 
 -- --------------------------------------------------------
 
@@ -145,9 +124,6 @@ CREATE TABLE `user` (
   `gender` varchar(10) NOT NULL,
   `teacherEmail` varchar(30) NOT NULL,
   `teacher_phoneNum` varchar(12) NOT NULL,
-  `teacher_Address` varchar(50) NOT NULL,
-  `date_of_birth` varchar(10) NOT NULL,
-  `acaQualification` varchar(20) NOT NULL,
   `teacherType` varchar(50) NOT NULL,
   `teacherSub1` varchar(50) NOT NULL,
   `teacherSub2` varchar(50) NOT NULL,
@@ -159,23 +135,13 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`tableID`, `login_id`, `teacherPassword`, `teacherName`, `gender`, `teacherEmail`, `teacher_phoneNum`, `teacher_Address`, `date_of_birth`, `acaQualification`, `teacherType`, `teacherSub1`, `teacherSub2`, `teacherSub3`, `class`) VALUES
-(1, '020822050567', '1234', '', 'MALE', '123', 'semalam', '020822050567', '0208220505', '020822050567', 'Guru kelas', 'Bahasa Melayu', '', '', '1 Bijak'),
-(2, '1234', '1234', '', 'Male', '', '', '', '', '', 'Guru subjek', 'Bahasa Inggeris', '', '', '4 Bijak'),
-(3, '4567', '4567', '', 'Male', '', '', '', '', '', 'Guru subjek', 'Sains', '', '', '1 Bijak'),
-(4, 'admin', 'admin', '', 'admin', 'admin', 'admin', 'admin', 'admin', 'admin', 'admin', 'admin', 'admin', 'admin', ''),
-(5, '222', 'meow', 'MEOW', 'MALE', '', '', '', '22', '', 'Guru kelas', 'Bahasa Melayu', '', '', '1 Bijak');
+INSERT INTO `user` (`tableID`, `login_id`, `teacherPassword`, `teacherName`, `gender`, `teacherEmail`, `teacher_phoneNum`, `teacherType`, `teacherSub1`, `teacherSub2`, `teacherSub3`, `class`) VALUES
+(1, 'admin', 'admin123', 'Admin SK Kota Raja', '', 'JBB5038@moe.edu.my', '069737536', 'admin', '', '', '', ''),
+(2, '020822050567', 'adam123', 'ADAM MIRZAN BIN AHMAD RIDZUAN', 'LELAKI', 'mirzan53@gmail.com', '0149454863', 'Guru kelas', 'Bahasa Inggeris', 'Matematik', '', '1 Bijak');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `report`
---
-ALTER TABLE `report`
-  ADD KEY `icNum` (`stuIC`),
-  ADD KEY `teacherName` (`teacherName`);
 
 --
 -- Indexes for table `student`
@@ -187,8 +153,7 @@ ALTER TABLE `student`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`tableID`),
-  ADD KEY `teacherName` (`teacherName`);
+  ADD PRIMARY KEY (`tableID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -198,13 +163,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `tableID` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `tableID` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `tableID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `tableID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
